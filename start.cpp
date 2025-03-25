10f059ff378e328f4b46481d5c98afc5af7d77ac
LETTTER <3536761163@qq.com>
2025-03-25 22:30:36 +0800

    add

--- start.cpp
+++ start.cpp
@@ -7,58 +7,54 @@ start::start(Widget *parent) : Widget(parent), stack(new QStackedWidget(this))
     // backbutton = new button("返回", this);
     // backbutton->move(0, 0);
 
-    // 创建四个按钮
-    startButton = new button("开始游戏", this);
-    aboutButton = new button("关于", this);
-    settingsButton = new button("设置", this);
-    exitButton = new button("退出", this);
+    // 创建 page1，并将四个按钮放入
+    QWidget *page1 = new QWidget();
 
+    startButton = new button("开始游戏", page1);
+    aboutButton = new button("关于", page1);
+    settingsButton = new button("设置", page1);
+    exitButton = new button("退出", page1);
 
+    // 退出按钮连接到窗口关闭
     QObject::connect(exitButton, &QPushButton::clicked, this, &QWidget::close);
 
-    // 创建网格布局管理器
+    // 创建按钮的网格布局
     QGridLayout *layout = new QGridLayout;
     layout->addWidget(startButton, 0, 0);
     layout->addWidget(aboutButton, 1, 0);
     layout->addWidget(settingsButton, 0, 1);
     layout->addWidget(exitButton, 1, 1);
 
-    // 创建一个 QWidget 作为中央部件
-    QWidget *buttonWidget = new QWidget(this);
+    // **创建一个 QWidget 作为中央部件**
+    QWidget *buttonWidget = new QWidget(page1);
     buttonWidget->setLayout(layout);
+    buttonWidget->setFixedSize(400, 200);  // 固定大小
 
-    // 调整按钮布局，使其居中
-    buttonWidget->setGeometry((this->width() - 400) / 2, (this->height() - 200) / 2, 400, 200);
+    // **创建一个布局让 buttonWidget 居中**
+    QVBoxLayout *page1Layout = new QVBoxLayout(page1);
+    page1Layout->addStretch();   // 上方弹性空间
+    page1Layout->addWidget(buttonWidget, 0, Qt::AlignCenter);
+    page1Layout->addStretch();   // 下方弹性空间
+    page1->setLayout(page1Layout);
 
-
-    //管理多个子窗口的显示
-    // 创建页面
-    QWidget *page1 = new QWidget();
+    // 创建其他页面
     QWidget *page2 = new QWidget();
     QWidget *page3 = new QWidget();
 
-    // 创建页面内容
-    QLabel *label1 = new QLabel("This is Page 1", page1);
     QLabel *label2 = new QLabel("This is Page 2", page2);
     QLabel *label3 = new QLabel("This is Page 3", page3);
 
-    // 创建切换页面的按钮
-    QPushButton *btnPage1 = new QPushButton("Go to Page 1", this);
-    QPushButton *btnPage2 = new QPushButton("Go to Page 2", this);
-    QPushButton *btnPage3 = new QPushButton("Go to Page 3", this);
-
-    // 将标签和按钮添加到每个页面
-    QVBoxLayout *layout1 = new QVBoxLayout(page1);
-    layout1->addWidget(label1);
-    layout1->addWidget(btnPage1);
+    QPushButton *btnPage1 = new QPushButton("Go to Page 1", page2);
+    QPushButton *btnPage2 = new QPushButton("Go to Page 2", page3);
+    QPushButton *btnPage3 = new QPushButton("Go to Page 3", page1);
 
     QVBoxLayout *layout2 = new QVBoxLayout(page2);
     layout2->addWidget(label2);
-    layout2->addWidget(btnPage2);
+    layout2->addWidget(btnPage1);
 
     QVBoxLayout *layout3 = new QVBoxLayout(page3);
     layout3->addWidget(label3);
-    layout3->addWidget(btnPage3);
+    layout3->addWidget(btnPage2);
 
     // 将页面添加到 QStackedWidget
     stack->addWidget(page1);
@@ -70,13 +66,13 @@ start::start(Widget *parent) : Widget(parent), stack(new QStackedWidget(this))
 
     // 设置切换按钮的功能
     connect(btnPage1, &QPushButton::clicked, [this]() {
-        this->stack->setCurrentIndex(1);
+        this->stack->setCurrentIndex(0);
     });
     connect(btnPage2, &QPushButton::clicked, [this]() {
-        this->stack->setCurrentIndex(2);
+        this->stack->setCurrentIndex(1);
     });
     connect(btnPage3, &QPushButton::clicked, [this]() {
-        this->stack->setCurrentIndex(0);
+        this->stack->setCurrentIndex(2);
     });
 
     // 布局：将 QStackedWidget 放入主窗口
