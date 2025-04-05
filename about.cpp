d00d9a31bd6624b49e388c0694fe3249d36bfc29
LETTTER <3536761163@qq.com>
2025-04-05 12:28:50 +0800

    about

--- about.cpp
+++ about.cpp
@@ -6,17 +6,7 @@ about::about(QWidget *parent):QWidget(parent)
     backButton = new button("返回", this);
     backButton->move(0, 0);
 
-    QVBoxLayout *layout = new QVBoxLayout(this);
 
-    button *loginButton = new button("Login", this);
-    button *registerButton = new button("Register", this);
-    button *guestButton = new button("Guest", this);
-
-    layout->addWidget(loginButton);
-    layout->addWidget(registerButton);
-    layout->addWidget(guestButton);
-
-    this->setLayout(layout);
 }
 
 about::~about()
